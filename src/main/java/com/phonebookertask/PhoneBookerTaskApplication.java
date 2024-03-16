fe3381c5f66745dd39420e299f1402129dacd6fe
Sabyrzhan Tynybayev <sabyrzhan1988@gmail.com>
2024-03-16 18:00:58 +0200

    Initial commit

---
+++ src/main/java/com/phonebookertask/PhoneBookerTaskApplication.java
@@ -0,0 +1,13 @@
+package com.phonebookertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookerTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneBookerTaskApplication.class, args);
+    }
+
+}
